be2ba1083d508a514ecf3f760c4db514fdf6afc2
Mark Scherer <euromark@web.de>
2016-01-17 23:58:39 +0100

    Init classes

---
+++ src/Model/Behavior/HashidBehavior.php
@@ -0,0 +1,181 @@
+<?php
+namespace Hashid\Model\Behavior;
+
+use Cake\Core\Configure;
+use Cake\ORM\Behavior;
+use Cake\ORM\Table;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
+use Cake\Event\Event;
+use \ArrayObject;
+use Hashids\Hashids;
+
+/**
+ * @author Mark Scherer
+ * @licence MIT
+ */
+class HashidBehavior extends Behavior {
+
+	const HID = 'hid';
+
+	/**
+	 * @var \Hashids\Hashids
+	 */
+	protected $hashids;
+
+	protected $_defaultConfig = [
+		'salt' => null, // Please provide your own salt via Configure
+		'field' => null, // To populate upon find() and save()
+		'tableField' => false, // To have a dedicated field in the table
+		'first' => false, // Either true or 'first' or 'firstOrFail'
+		'implementedFinders' => [
+			'hashed' => 'findHashed',
+		]
+	];
+
+	/**
+	 * Constructor
+	 *
+	 * Merges config with the default and store in the config property
+	 *
+	 * Does not retain a reference to the Table object. If you need this
+	 * you should override the constructor.
+	 *
+	 * @param Table $table The table this behavior is attached to.
+	 * @param array $config The config for this behavior.
+	 */
+	public function __construct(Table $table, array $config = []) {
+		$defaults = (array)Configure::read('Hashid');
+		parent::__construct($table, $config + $defaults);
+
+		$this->_table = $table;
+	}
+
+	/**
+	 * @param Event $event
+	 * @param Query $query
+	 * @return void
+	 */
+	public function beforeFind(Event $event, Query $query) {
+		$field = $this->_config['field'];
+		if (!$field) {
+			return;
+		}
+
+		$idField = $this->_table->primaryKey();
+
+		$mapper = function ($row, $key, $mr) use ($field, $idField) {
+			$row->set($field, $this->encodeId($row->get($idField)));
+			$mr->emit($row);
+		};
+		$query->mapReduce($mapper);
+	}
+
+	/**
+	 * @param \Cake\Event\Event $event The beforeSave event that was fired
+	 * @param \Cake\ORM\Entity $entity The entity that is going to be saved
+	 * @param \ArrayObject $options the options passed to the save method
+	 * @return void
+	 */
+	public function afterSave(Event $event, Entity $entity, ArrayObject $options) {
+		if (!$entity->isNew()) {
+			return;
+		}
+
+		if ($this->encode($entity)) {
+			$this->_table->save($entity, ['validate' => false]);
+		}
+	}
+
+	/**
+	 * Sets up hashid for model.
+	 *
+	 * @param \Cake\ORM\Entity $entity The entity that is going to be saved
+	 * @return bool True if save should proceed, false otherwise
+	 */
+	public function encode(Entity $entity) {
+		$idField = $this->_table->primaryKey();
+		$id = $entity->get($idField);
+		if (!$id) {
+			return false;
+		}
+
+		$field = $this->_config['field'];
+		$tableField = $this->_config['tableField'];
+
+		if ($tableField === true) {
+			$tableField = $field;
+		}
+		if (!$field && !$tableField) {
+			return false;
+		}
+
+		$hashid = $this->encodeId($id);
+		if ($field) {
+			$entity->set($field, $hashid);
+		}
+		if ($tableField && $tableField !== $field) {
+			$entity->set($tableField, $hashid);
+		}
+
+		return true;
+	}
+
+	/**
+	 * @return \Hashids\Hashids
+     */
+	protected function getHasher() {
+		if (isset($this->hashids)) {
+			return $this->hashids;
+		}
+		$this->hashids = new Hashids($this->_config['salt']);
+
+		return $this->hashids;
+	}
+
+	/**
+	 * Custom finder for hashids field.
+	 *
+	 * Options:
+	 * - hid (required), best to use HashidBehavior::HID constant
+	 * - noFirst (optional, to leave the query open for adjustments, no first() called)
+	 *
+	 * @param \Cake\ORM\Query $query Query.
+	 * @param array $options Array of options as described above
+	 * @return \Cake\ORM\Query
+	 */
+	public function findHashed(Query $query, array $options) {debug($query);
+		$tableField = $this->_config['tableField'];
+		if ($tableField) {
+			$query->where([$tableField => $options[HashidBehavior::HID]]);
+		} else {
+			$idField = $this->_table->primaryKey();
+			$id = $this->decodeHashid($options[HashidBehavior::HID]);
+			$query->where([$idField => $id]);
+		}
+
+		$first = $this->_config['first'] === true ? 'first' : $this->_config['first'];
+		if (!$first || !empty($options['noFirst'])) {
+			return $query;
+		}
+		return $query->first();
+	}
+
+	/**
+	 * @param int $id
+	 * @return string
+     */
+	public function encodeId($id) {
+		return $this->getHasher()->encode($id);
+	}
+
+	/**
+	 * @param string $hashid
+	 * @return int
+	 */
+	public function decodeHashid($hashid) {
+		$ids = $this->getHasher()->decode($hashid);
+		return array_shift($ids);
+	}
+
+}
---
+++ tests/TestCase/Model/Behavior/HashidBehaviorTest.php
@@ -0,0 +1,181 @@
+<?php
+namespace Hashid\Test\Model\Behavior;
+
+use Cake\Controller\Controller;
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\TestSuite\TestCase;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Entity;
+use Cake\Database\Driver\Mysql;
+use Hashid\Model\Behavior\HashidBehavior;
+use Hashids\Hashids;
+
+class HashidBehaviorTest extends TestCase {
+
+	public $fixtures = [
+		'plugin.Hashid.Addresses'
+	];
+
+	/**
+	 * @var \Cake\ORM\Table;
+	 */
+	public $Addresses;
+
+	/**
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Addresses = TableRegistry::get('Hashid.Addresses');
+		$this->Addresses->addBehavior('Hashid.Hashid');
+	}
+
+	/**
+	 * @return void
+	 */
+	public function tearDown() {
+		parent::tearDown();
+
+		unset($this->Addresses);
+		TableRegistry::clear();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testSave() {
+		$data = [
+			'city' => 'Foo'
+		];
+		$address = $this->Addresses->newEntity($data);
+		$res = $this->Addresses->save($address);
+		$this->assertTrue((bool)$res);
+
+		$this->assertNull($address->hash);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testFind() {
+		$data = [
+			'city' => 'Foo'
+		];
+		$address = $this->Addresses->newEntity($data);
+		$res = $this->Addresses->save($address);
+
+		$id = $address->id;
+		$hasher = new Hashids();
+		$hashid = $hasher->encode($id);
+
+		$address = $this->Addresses->find('hashed', [HashidBehavior::HID => $hashid]);
+		$this->assertTrue((bool)$address);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testSaveWithField() {
+		$this->Addresses->behaviors()->Hashid->config('field', 'hash');
+
+		$data = [
+			'city' => 'Foo'
+		];
+		$address = $this->Addresses->newEntity($data);
+		$res = $this->Addresses->save($address);
+		$this->assertTrue((bool)$res);
+
+		$hasher = new Hashids();
+		$expected = $hasher->encode($address->id);
+		$this->assertEquals($expected, $address->hash);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testFindHashedWithField() {
+		$this->Addresses->behaviors()->Hashid->config('field', 'hash');
+
+		$data = [
+			'city' => 'Foo'
+		];
+		$address = $this->Addresses->newEntity($data);
+		$res = $this->Addresses->save($address);
+
+		$hashid = $address->hash;
+
+		$address = $this->Addresses->find('hashed', [HashidBehavior::HID => $hashid])->first();
+		$this->assertTrue((bool)$address);
+	}
+
+	/**
+		 * @return void
+		 */
+		public function testFindWithField() {
+			$address = $this->Addresses->find()->where(['city' => 'NoHashId'])->first();
+			$hashid = $this->Addresses->encodeId($address->id);
+
+			$this->Addresses->behaviors()->Hashid->config('field', 'hash');
+
+			// Should also be included now
+			$address = $this->Addresses->find()->where(['city' => 'NoHashId'])->first();
+			$this->assertSame($hashid, $address->hash);
+
+			// Should also be included now
+			$address = $this->Addresses->get($address->id);
+			$this->assertSame($hashid, $address->hash);
+		}
+
+	/**
+	 * @return void
+	 */
+	public function testSaveWithTableField() {
+		$this->Addresses->behaviors()->Hashid->config('tableField', 'hash');
+
+		$data = [
+			'city' => 'FooBar'
+		];
+		$address = $this->Addresses->newEntity($data);
+		$res = $this->Addresses->save($address);
+		$this->assertTrue((bool)$res);
+
+		$hasher = new Hashids();
+		$expected = $hasher->encode($address->id);
+		$this->assertEquals($expected, $address->hash);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testFindByTableField() {
+		$this->Addresses->behaviors()->Hashid->config('tableField', 'hash');
+
+		$hash = 'jR';
+		$address = $this->Addresses->find('hashed', [HashidBehavior::HID => $hash])->first();
+		$this->assertTrue((bool)$address);
+
+		$hasher = new Hashids();
+		$ids = $hasher->decode($address->hash);
+		$id = array_shift($ids);
+		$this->assertEquals($address->id, $id);
+	}
+
+	/**
+ * @return void
+ */
+	public function testFindHashed() {
+		$address = $this->Addresses->find('hashed', [HashidBehavior::HID => 'jR'])->firstOrFail();
+		$this->assertTrue((bool)$address);
+	}
+
+	/**
+	 * @expectedException \Cake\Datasource\Exception\RecordNotFoundException
+	 * @return void
+	 */
+	public function testFindHashedFail() {
+		$this->Addresses->find('hashed', [HashidBehavior::HID => 'jRx'])->firstOrFail();
+	}
+
+}
